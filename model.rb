7c79161e8b7ba913596d1c0537006399731ed656
Sebastian Thörn <thorn@p0int3r.se>
2020-03-12 23:42:14 +0000

    Adds model

---
+++ model.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+class Model
+
+  #
+  # initialize, runs at start
+  def initialize(username, password)
+
+    #
+    # Set username and password if needed
+    @username = username
+    @password = password
+
+  end
+
+end
